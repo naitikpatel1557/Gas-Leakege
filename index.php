04dbf3f0a9ce4f8fa06052eac0d64f8e84828ac9
naitikpatel1557 <akikpatel1556@gmail.com>
2025-11-25 22:01:33 +0530

    Fixed secret and updated code

--- index.php
+++ index.php
@@ -4,8 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IoT Gas Leakage Monitor</title>
+
+    <!-- Fevicon Icon -->
+    <link rel="icon" type="image/x-icon" href="Favicon Icon/Icon96git reset --soft HEAD~11.png">
+    
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js"></script>
+    
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;800&display=swap');
         body { font-family: 'Inter', sans-serif; }
